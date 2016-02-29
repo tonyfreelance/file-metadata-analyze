d20bb7f52f0861fa709eab285d8bb0358f2bad53
tonyfreelance <tony@tonyfreelance.com>
2016-02-29 02:31:45 +0000

    First init

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,35 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var moment = require("moment")
 
-module.exports = function (app, passport) {
-
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
-
-	var clickHandler = new ClickHandler();
+module.exports = function(app, passport) {
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function(req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+	app.route('/:timestamp')
+		.get(function(req, res) {
+			var timestamp = {
+				unix: null,
+				natural: null
+			}
+			var inputTime = req.params.timestamp
+			var time;
+			// If input is unix timestamp
+			if(moment.unix(inputTime).isValid()) {
+				timestamp.unix = JSON.parse(inputTime)
+				timestamp.natural = moment.unix(inputTime).format("MMMM DD, YYYY")
+			} 
+			// If input is natural human-readable date
+			else if(moment(inputTime).isValid()) {
+				timestamp.unix = moment(inputTime, "MMMM DD, YYYY").unix()
+				timestamp.natural = moment(inputTime).format("MMMM DD, YYYY")
+			} 
+			
+			res.json(timestamp)
+			
 		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
 };
